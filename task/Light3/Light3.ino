a4d8dda794984ad5bea4dde7859564b418ee1f3c
chenxinfeng4 <529125853@qq.com>
2017-12-06 19:15:24 +0800

    Fix the demo task

--- task/Light3/Light3.ino
+++ task/Light3/Light3.ino
@@ -10,7 +10,7 @@ const char taskName[] = __FILE__;
 #define UNO_SPEEDUP //only helpful to ArControl_AllinOne.h, improve AI-scaning
 #define AI2IN 1		//AIx -> INy
 #define DO2OUT -1	//DOx -> OUTy
-#include "E:/ArControl_ReportCollection/ArControl_Qt_Code/release/ino/ArControl_AllinOne.h"
+#include "E:/ArControl_ReportCollection/ArControl_github_realse/ino/ArControl_AllinOne.h"
 
 ///////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////global vars/////////////////////////////////////////
@@ -58,19 +58,19 @@ void State_SETUP()
 	int i, j=0;
 	for(i=1; i<=numC1; ++i)
 		C1S[i]=stateList[j++].setCiSi(1,i); //pick "* State" one by one
-	C1S[0] = C[0]; //End of Session
+	C1S[0] = C[0] = State::ENDSTATE;//End of Session
 	C[1] = C1S[1]; //Entry of Component
 	State::NEXTSTATE = C[1];//Entry of Session
 ///Component [1]:Light3 (loop=10)
 	///C1S[1]: Swither
 	C1S[1]->CountSet = []()-> int {return 10;}; //n=10 -> STOP
 	C1S[1]->CountSetSTATE = []()-> State* {int n=0; return C1S[n];};
-	C1S[1]->TimerSet = []()-> float {return 3;}; //3
+	C1S[1]->TimerSet = []()-> float {return 3;}; //t=3sec -> S3
 	C1S[1]->TimerSetSTATE = []()-> State* {int n=3; return C1S[n];};
 	C1S[1]->evtListener = []()-> bool {return cpp_ListenAI(IN1);}; //leftPoke -> S2
 	C1S[1]->evtListenerSTATE = []()-> State* {int n=2; return C1S[n];};
 	C1S[1]->addlisten();
-	C1S[1]->evtListener = []()-> bool {return cpp_ListenAI(IN1);}; //rightPoke-> S4
+	C1S[1]->evtListener = []()-> bool {return cpp_ListenAI(IN2);}; //rightPoke-> S4
 	C1S[1]->evtListenerSTATE = []()-> State* {int n=4; return C1S[n];};
 	C1S[1]->addlisten();
 	///C1S[2]: Green 3sec
