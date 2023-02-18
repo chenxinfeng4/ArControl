44fcd40b7db6cc4f2fb7dc3bd9c3347e2a1088d2
ChenXF <529125853@qq.com>
2023-02-18 13:22:06 +0800

    modify pytools

--- pytools/BF_arc2mat_proxy.py
+++ pytools/BF_arc2mat_proxy.py
@@ -1,10 +1,13 @@
-# pyinstaller BF_arc2mat_proxy.py --name BF_arc2mat --upx-exclude BF_arc2mat.py --add-data BF_arc2mat.py;src 
+# pyinstaller BF_arc2mat_proxy.py --name BF_arc2mat --exclude-module BF_arc2mat --upx-exclude BF_arc2mat.py --add-data BF_arc2mat.py;src 
 # This's a pyinstaller proxy of BF_arc2mat.py, so the src/BF_arc2mat.py can be upgrade.
 import re
 import mat4py as sio
 import sys
+import os
 
-sys.path.append('./src')
+if __name__ == '__main__':
+    path = os.path.abspath(os.path.dirname(sys.argv[0]))
+    sys.path.append(os.path.join(path, 'src'))
 
 from BF_arc2mat import convert_txt2mat
 
